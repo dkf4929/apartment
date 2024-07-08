f760fa24397df23261fdb02afb3445bf43697722
yeonghoS <dkf4928@naver.com>
2024-07-08 18:03:02 +0900

    feat: 회원 저장 기능

--- src/main/java/com/project/apartment/domain/member/MemberService.java
+++ src/main/java/com/project/apartment/domain/member/MemberService.java
@@ -1,10 +1,28 @@
 package com.project.apartment.domain.member;
 
+import com.project.apartment.domain.member.dto.MemberSaveDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void saveMember(MemberSaveDto memberSaveDto) {
+        Member member = Member.createMember(memberSaveDto.getLoginId(),
+                passwordEncoder.encode(memberSaveDto.getPassword()),
+                passwordEncoder.encode(memberSaveDto.getRegNo()),
+                memberSaveDto.getName(),
+                memberSaveDto.getGender(),
+                memberSaveDto.getAddress(),
+                memberSaveDto.getBirthDate(),
+                List.of("ROLE_USER"));
+
+        memberRepository.save(member);
+    }
 }
