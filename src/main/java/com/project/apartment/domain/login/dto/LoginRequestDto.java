5fafbecea565b8e935996a6d681434689bfb25ac
dkf4929 <sng4929@naver.com>
2024-07-08 18:03:12 +0900

    feat: 로그인용 dto

--- src/main/java/com/project/apartment/domain/login/dto/LoginRequestDto.java
+++ src/main/java/com/project/apartment/domain/login/dto/LoginRequestDto.java
@@ -1,9 +1,13 @@
 package com.project.apartment.domain.login.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class LoginRequestDto {
+    @NotBlank
     private String loginId;
+
+    @NotBlank
     private String password;
 }
