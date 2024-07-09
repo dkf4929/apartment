ba301deb7b73750ba8256d745f226b9df8f80483
dkf4929 <sng4929@naver.com>
2024-07-09 14:50:36 +0900

    feat: 로그인 관련 처리

--- src/main/java/com/project/apartment/domain/login/dto/LoginRequestDto.java
+++ src/main/java/com/project/apartment/domain/login/dto/LoginRequestDto.java
@@ -1,13 +1,16 @@
 package com.project.apartment.domain.login.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class LoginRequestDto {
     @NotBlank
+    @Schema(description = "로그인 ID", example = "GILDONG1")
     private String loginId;
 
     @NotBlank
+    @Schema(description = "비밀번호", example = "qwer1234")
     private String password;
 }
