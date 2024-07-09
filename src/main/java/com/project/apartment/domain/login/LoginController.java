db45f1f9ad0df1026d98728c8a905b2c2771921e
dkf4929 <sng4929@naver.com>
2024-07-09 14:50:27 +0900

    feat: 로그인 관련 처리

--- src/main/java/com/project/apartment/domain/login/LoginController.java
+++ src/main/java/com/project/apartment/domain/login/LoginController.java
@@ -2,9 +2,14 @@ package com.project.apartment.domain.login;
 
 import com.project.apartment.domain.login.dto.LoginRequestDto;
 import com.project.apartment.domain.login.dto.LoginResponseDto;
-import com.project.apartment.domain.member.Member;
+import com.project.apartment.domain.user.member.Member;
 import com.project.apartment.global.dto.JwtTokenDto;
 import com.project.apartment.global.jwt.JwtTokenProvider;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +18,7 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +31,19 @@ public class LoginController {
     private final LoginService loginService;
     private final JwtTokenProvider jwtTokenProvider;
 
+    @Operation(summary = "로그인", description = "커뮤니티에 로그인 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(
+                    mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 입력값입니다.", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(example = "{ \"message\": \"올바른 입력값을 입력하세요.\" }"))),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(example = "{ \"message\": \"서버 오류 발생.\" }"))),
+    })
     @PostMapping
-    public LoginResponseDto login(HttpServletResponse response, @Valid LoginRequestDto loginRequestDto) {
+    public LoginResponseDto login(HttpServletResponse response, @RequestBody @Valid LoginRequestDto loginRequestDto) {
         // 로그인 인증 처리
         Member loginMember = loginService.login(loginRequestDto);
 
--- src/main/java/com/project/apartment/domain/login/LoginService.java
+++ src/main/java/com/project/apartment/domain/login/LoginService.java
@@ -1,8 +1,8 @@
 package com.project.apartment.domain.login;
 
 import com.project.apartment.domain.login.dto.LoginRequestDto;
-import com.project.apartment.domain.member.Member;
-import com.project.apartment.domain.member.MemberRepository;
+import com.project.apartment.domain.user.member.Member;
+import com.project.apartment.domain.user.member.MemberRepository;
 import com.project.apartment.global.exception.MemberNotFoundException;
 import com.project.apartment.global.exception.PasswordNotMatchException;
 import lombok.RequiredArgsConstructor;
@@ -19,11 +19,13 @@ public class LoginService {
         String loginId = loginRequestDto.getLoginId();
         String password = loginRequestDto.getPassword();
 
+        // 회원 검색
         Member findMember = memberRepository.findByLoginId(loginId)
                 .orElseThrow(() -> new MemberNotFoundException("등록되지 않은 사용자 입니다."));
 
+        // 패스워드 일치여부 검사
         boolean passwordMatch
-                = passwordEncoder.matches(findMember.getPassword(), password);
+                = passwordEncoder.matches(password, findMember.getPassword());
 
         if (!passwordMatch)
             throw new PasswordNotMatchException("비밀번호가 일치하지 않습니다.");
