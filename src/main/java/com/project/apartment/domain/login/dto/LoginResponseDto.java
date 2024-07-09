6fe10d9a811e6c232f420474239dbe9500387058
dkf4929 <sng4929@naver.com>
2024-07-09 14:50:36 +0900

    feat: 로그인 관련 처리

--- src/main/java/com/project/apartment/domain/login/dto/LoginResponseDto.java
+++ src/main/java/com/project/apartment/domain/login/dto/LoginResponseDto.java
@@ -1,15 +1,20 @@
 package com.project.apartment.domain.login.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class LoginResponseDto {
+    @Schema(description = "액세스 토큰")
     private String accessToken;
 
+    @Schema(description = "토큰 만료시간")
     private Long expiresIn;
 
+    @Schema(description = "refreshToken 만료시간")
     private Long refreshTokenExpiresIn;
 
+    @Schema(description = "토큰 타입")
     private String tokenType;
 
     public static LoginResponseDto of(String accessToken, long expiresIn, long refreshTokenExpiresIn, String tokenType) {
